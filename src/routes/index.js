d293c92e882ec6b732afa82247ae6c50f5c08397
PikaCode5900 <pableitor5900@gmail.com>
2023-02-21 23:53:03 +0100

    Probando las vistas con PUG *2

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.render('index.pug', { mensaje: 'Usando Pug JS en Express' }); // Se muestra la plantilla hello.pug
+    res.render('index.pug', { titulo: 'PUG Page' ,mensaje: 'Usando Pug JS en Express' }); // Se muestra la plantilla hello.pug
 });
 
 module.exports = router
