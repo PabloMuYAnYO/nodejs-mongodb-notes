ab8bc93a51279091cc25caa505a77a6cecbbb1ca
PikaCode5900 <pableitor5900@gmail.com>
2023-02-21 17:37:29 +0100

    Probando conexión con la base de datos *2

--- src/dbConnection.js
+++ src/dbConnection.js
@@ -1,12 +1,22 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.set('strictQuery', false);
+mongoose.Promise = global.Promise;
+
 
 // Connection
 const { MONGODB_HOST, MONGODB_DATABASE } = process.env;
 
-const MONGODB = `mongodb://${ MONGODB_HOST}/${ MONGODB_DATABASE }`
+const MONGODB = `mongodb://${ MONGODB_HOST}/${ MONGODB_DATABASE }`;
 
-mongoose.connect(MONGODB)
-    .then( () => { console.log('Connected with mongo'); } )
-    .catch( (err) => console.log(err) )
+// Connect MongoDB at default port 27017.
+mongoose.connect(MONGODB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}, (err) => {
+    if (!err) {
+        console.log('MongoDB Connection Succeeded.')
+    } else {
+        console.log('Error in DB connection: ' + err)
+    }
+});
