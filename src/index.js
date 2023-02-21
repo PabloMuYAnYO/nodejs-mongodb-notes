42b24eb9d98fb7be212ff39a1bd7ccec3831bbeb
PikaCode5900 <pableitor5900@gmail.com>
2023-02-21 14:46:01 +0100

    Servidor con configuraciones básicas y  funcionando correctamente

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 // * ARCHIVO PRINCIPAL DE ARRANQUE DE LA APLICACIÓN
 
+const app = require('./server');
+const colors = require('colors');
+
+
+
+app.listen(app.set('port'), () => {
+    console.log('Server on port:'.blue +  ` ${app.get('port')}`.yellow);
+});
---
+++ src/server.js
@@ -0,0 +1,33 @@
+// ? El archivo server.js contiene el código de JavaScript para iniciar un servidor con nuestra aplicación y responder a las solicitudes.
+
+const express = require('express');
+const path = require('path'); // * Modulo por defecto de Nodejs, en path se especifican las rutas en las cuales el intérprete de comandos debe buscar los programas a ejecutar.
+
+// * INITIALIZATIONS * \\
+// express framework
+const app = express();
+
+// * SETTINGS * \\
+// --> port
+app.set('port', process.env.PORT || 3000);
+
+// --> views dir and engine
+app.set('views', path.join(__dirname, 'views'))
+
+// --> middleware's
+app.use(express.urlencoded({extended:false}));
+
+// --> global vars
+
+
+// --> routes
+app.get('/', (req, res) => {
+    res.send('<h1>Hola Mundo!!!</h1>');
+});
+
+
+// --> static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+module.exports = app;
